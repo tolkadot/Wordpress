a8ebe8f310f10d5ecea3c523e27cd63f39c0fb07
Dee Bryant <tolkadot@gmail.com>
2016-11-28 21:17:29 +1100

    Create dummy-home.php

---
+++ YITH-Add-to-quote/dummy-home.php
@@ -0,0 +1,11 @@
+<section>
+			<div class="col-xs-12 col-sm-8 col-sm-offset-1">
+				
+    <div class="yith-ywraq-add-to-quote add-to-quote-61">
+    <div class="yith-ywraq-add-button show" style="display:block">
+        
+    <a href="#" class="add-request-quote-button button" data-product_id="61" data-wp_nonce="78ea6e85d9">Get me a quote</a>
+    </div>
+</div>
+</div>
+</section>
