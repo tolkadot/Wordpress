10853eedefc53c18f4d50d4ba0f860cbbefd93b7
Dee Bryant <tolkadot@gmail.com>
2017-05-20 13:44:47 +1000

    Create copyright-snippet-shortcode.php

---
+++ snippets/copyright-snippet-shortcode.php
@@ -0,0 +1,10 @@
+//copyright 
+function tolka_copyright() {
+
+$today = getdate();
+$thisyear = $today[year];
+$copyright = "&copy; " . $thisyear . " KBS Engineering";
+return $copyright;
+
+}
+add_shortcode( 'do_copyright', 'tolka_copyright' );
