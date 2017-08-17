fa48592ac77ad74e050b67c424c17414a97cfa86
Dee Bryant <tolkadot@gmail.com>
2017-08-18 09:58:22 +1000

    Create enqueue-custom-script.php

---
+++ gravity forms/enqueue-custom-script.php
@@ -0,0 +1,4 @@
+add_action( 'gform_enqueue_scripts', 'tolka_custom_script', 10, 2 );
+function tolka_custom_script( $form) {
+wp_enqueue_script( 'tolkajs', get_stylesheet_directory_uri(). '/js/gravity-snippets.js', array(), '20170722' );
+}
